16ab879c8b9205b384b9f478a33a73650eaa9e53
phoenix <175668151+phoenixlovesu@users.noreply.github.com>
2025-02-13 10:47:06 -0500

    setup buttons and setting area; initialized variables

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-(function()
-{
+(function(){
+
     /**
      * Declare vars
      */
     const body = document.body;
-    /**
-     * 
-     */
+    const workDurationInput = document.getElementById('work-duration');
+    const restDurationInput = document.getElementById('rest-duration');
+    const timerTime = document.getElementById('timer-time');
+
+    let workDuration = parseInt(workDurationInput.value) * 60;
+    let restDuration = parseInt(restDurationInput.value) * 60;
+    letRemainingTime = workDuration;
+    let isPaused = true;
+    let isWorking = true;
+    let intervalId;
+
+
+
     window.addEventListener("load",() => {
-        body.classList.add('page-loaded');
+        body.classList.add('page-loaded'); //hides overlay and reveals timer 
     });
-}
-)();
+})();
