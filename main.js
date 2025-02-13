6abe21cef266ae4b0d64e017fb9072be25944251
phoenix <175668151+phoenixlovesu@users.noreply.github.com>
2025-02-13 06:39:34 -0500

    set up overlay, timer progress & completed area

---
+++ main.js
@@ -0,0 +1,14 @@
+(function()
+{
+    /**
+     * Declare vars
+     */
+    const body = document.body;
+    /**
+     * 
+     */
+    window.addEventListener("load",() => {
+        body.classList.add('page-loaded');
+    });
+}
+)();
